344443ce85f4c8e8f54bc4c66ec3d03d9516f79b
TheDoctor <TheDoctor@Gallifrey.org>
2021-03-24 09:45:26 -0700

    Add an example of binding a list item name to a single property.

--- WPFListItemAccessibleNames/MainWindow.xaml.cs
+++ WPFListItemAccessibleNames/MainWindow.xaml.cs
@@ -12,15 +12,20 @@ namespace WPFListItemAccessibleNames
         {
             InitializeComponent();
 
-            List<Bird> items = new List<Bird>();
-            items.Add(new Bird() { Name = "House Sparrow", Habitat = "Cities, suburbs, farms", Voice = "Repeated series of chirrup sounds"});
-            items.Add(new Bird() { Name = "Golden-crowned Sparrow", Habitat = "Brushy places, including neighborhoods", Voice = "Series of long, raspy, whistled notes" });
-            items.Add(new Bird() { Name = "Song Sparrow", Habitat = "Found throughout Puget Sound Region, up to mountain passes", Voice = "Song begins with several clear notes, followed by lower note, jumbled trill" });
-            BirdList.ItemsSource = items;
+            List<Animal> dogs = new List<Animal>();
+            dogs.Add(new Animal() { Name = "Spaniel", Habitat = "Lounge and garden", Voice = "Woof" });
+            dogs.Add(new Animal() { Name = "Labrador", Habitat = "Kitchen", Voice = "Woof" });
+            DogList.ItemsSource = dogs;
+
+            List<Animal> birds = new List<Animal>();
+            birds.Add(new Animal() { Name = "House Sparrow", Habitat = "Cities, suburbs, farms", Voice = "Repeated series of chirrup sounds"});
+            birds.Add(new Animal() { Name = "Golden-crowned Sparrow", Habitat = "Brushy places, including neighborhoods", Voice = "Series of long, raspy, whistled notes" });
+            birds.Add(new Animal() { Name = "Song Sparrow", Habitat = "Found throughout Puget Sound Region, up to mountain passes", Voice = "Song begins with several clear notes, followed by lower note, jumbled trill" });
+            BirdList.ItemsSource = birds;
         }
     }
 
-    public class Bird
+    public class Animal
     {
         public string Name { get; set; }
         public string Habitat { get; set; }
